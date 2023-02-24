3fb463694eb60ff173db8345f1a6420170ef112a
tatsu-m <tatsuhiro.0130@gmail.com>
2023-02-24 13:09:17 +0900

    ラジオボタン情報取得のjs作成

--- js/survey.js
+++ js/survey.js
@@ -2,9 +2,31 @@
 
 {
 //フォームの内容をとってくる（たつ）
+document.querySelector('#submit').addEventListener('click', () => {
+  const element =document.forms.survey;
 
-}
+  //名前と期生取得
+  const nameValue = element.name.value;
+  const gradeValue = element.grade.value;
 
+  console.log(nameValue)
+  console.log(gradeValue)
+
+  //朝夜内向型取得
+  const timingValue = element.timing.value;
+  const personalityValue = element.personality.value;
+
+  console.log(timingValue)
+  console.log(personalityValue)
+
+
+ //趣味
+  Array.prototype.forEach.call(formElements.checkbox, function (checkbox) {
+    if(checkbox.checked === true){
+      console.log('Checkbox: ', checkbox.value);
+    }
+  });
+});
 {
 //モーダルを作る（咲乃）
 const modal = document.querySelector(".modal_wrapper");
@@ -21,4 +43,5 @@ modalOpen.addEventListener('click', e => {
   });
 
 
+}
 }
