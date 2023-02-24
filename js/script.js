4bb042c42c9cc185a12745072c5e344fdedfe03d
Sakino Miki <sakinom328@gmail.com>
2023-02-24 10:47:02 +0900

    jsのファイル名を変更

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 'use strict';
 
 {
-//フォームの内容をとってくる（日向）
+//フォームの内容をとってくる（たつ）
 
 }
 
 {
 //モーダルを作る（咲乃）
+const modal = document.querySelector(".modal_wrapper");
+
+const modalOpen = document.getElementById("submit");
+modalOpen.addEventListener('click', e => {
+  e.preventDefault();
+
+  modal.style.display = 'flex';
+})
+
+  $('.modal_close').on('click', () => {
+    $('.modal_wrapper').css('display', 'none');
+  });
+
 
 }
